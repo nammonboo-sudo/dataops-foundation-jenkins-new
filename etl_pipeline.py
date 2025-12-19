435702d4c710304f778529dd7148af3b39de51ad
Nammon Boo <nammon.boo@gmail.com>
2025-12-19 11:52:12 +0700

    update

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -139,7 +139,7 @@ def deploy_to_database(fact_table, dim_tables):
     print("\n🚀 Deploying to Database...")
     
     # Database configuration
-    server = "36.111.96.104"
+    server = "136.111.96.104"
     database = 'TestDB'
     username = 'SA'
     password = os.getenv('DB_PASSWORD', 'Passw0rd123456')
